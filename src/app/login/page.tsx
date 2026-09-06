9f4da69c7a240e8f60b178ce0c35ff123c4f4710
saniatariq388 <saniatariq388@gmail.com>
2026-09-07 04:54:50 +0500

    build issue resolved

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Login from "@/components/login";
 
 export default function LoginPage() {
-  return <Login />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  );
 }
